4bc637003195bd8798c7f354c4a3ccd01b1588c8
alpha-tango-kilo <git@heyatk.com>
2024-05-17 19:16:47 +0100

    Document existing XML API

--- src/xml/errors.rs
+++ src/xml/errors.rs
@@ -4,6 +4,18 @@ use thiserror::Error;
 
 use crate::xml::lexer::PlistTag;
 
+/// The error encountered when parsing a [`Value`](crate::Value) or
+/// [`XmlDocument`](super::XmlDocument), annotated with the location in the
+/// original source
+///
+/// It is highly recommended to use this crate/error with
+/// [`miette`](https://lib.rs/crates/miette), as this error implements
+/// [`Diagnostic`]. This can produce an annotated snippet of the source code to
+/// show you exactly where the error occurred
+///
+/// If you don't wish to use `miette`, you should access/print the inner type
+/// with [`kind()`](XmlParseSourceError::kind) to get a more useful error to
+/// display than just "failed to parse XML"
 #[derive(Debug, Error, Diagnostic)]
 #[error("failed to parse XML")]
 #[cfg_attr(test, diagnostic(help("this is probably a bug your dumbass wrote")))]
@@ -23,14 +35,26 @@ pub struct XmlParseSourceError<'a> {
     pub(crate) span: Option<SourceSpan>,
 }
 
+impl XmlParseSourceError<'_> {
+    /// Access the inner error without the source code and span bundled with it
+    pub fn kind(&self) -> XmlErrorType {
+        self.inner
+    }
+}
+
+/// XML lexing/parsing errors containing a span but not the source
+///
+/// Not public facing, always converted to a [`XmlParseSourceError`]
 #[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub(crate) struct XmlError(XmlErrorType, Option<Span>);
 
 impl XmlError {
+    /// Create a new spanless `XmlError`
     pub(crate) const fn new(r#type: XmlErrorType) -> Self {
         Self(r#type, None)
     }
 
+    /// Convert into a [`XmlParseSourceError`] by providing the source
     pub(crate) fn with_source(self, source: &str) -> XmlParseSourceError {
         let XmlError(inner, span) = self;
         XmlParseSourceError {
@@ -41,42 +65,64 @@ impl XmlError {
     }
 }
 
+/// The underlying error encountered when parsing a [`Value`](crate::Value) or
+/// [`XmlDocument`](super::XmlDocument)
 #[derive(Debug, Error, Copy, Clone, Default, PartialEq, Eq)]
 pub enum XmlErrorType {
+    /// Unlexable content - something is so wrong I don't even know what I'm
+    /// looking at
     #[default]
     #[error("unlexable content")]
     Unlexable,
+    /// An empty tag for something that doesn't make sense to be empty, e.g.
+    /// `<integer/>`
     #[error("an empty {0} doesn't make sense")]
     WeirdEmpty(PlistTag),
+
     // Only used by pushers/poppers
+    /// Mismatched open & close tags
     #[error("mismatched open & close tags: <{0}>...</{1}>")]
     MismatchedOpenClose(PlistTag, PlistTag),
+    /// Close tag without open tag
     #[error("closing </{0}> with no opening tag")]
     LonelyClose(PlistTag),
+
     // Only used by gobblers
+    /// Unclosed tag
     #[error("unclosed <{0}>")]
     Unclosed(PlistTag),
+    /// Unparseable tag contents, e.g. `<integer>elephant</integer>`
     #[error("could not parse as {0}")]
     CouldNotParse(PlistTag),
+
     // Only used by collections
+    /// Collections more deeply nested than `plist-pls` supports (58-deep)
     #[error(
         "collections are too deeply nested, only 58-deep collections supported"
     )]
     WeAreInTooDeep,
+
     // Higher-level parser errors (not detected by the lexer)
+    /// Missing key in a dictionary
     #[error("needed key")]
     MissingKey,
+    /// Wanted a [`Value`](crate::Value), got something else
     #[error("expected value")]
     ExpectedValue,
+    /// Unexpected EOF
     #[error("input ended unexpectedly")]
     UnexpectedEnd,
+    /// Unwanted extra content
     #[error("unwanted extra content")]
     ExpectedEnd,
+    /// Missing XML plist headers ([`XmlDocument`](crate::xml::XmlDocument)
+    /// only)
     #[error("missing XML plist headers")]
     MissingHeader,
 }
 
 impl XmlErrorType {
+    /// Annotate an error with the span in the source that it occurred at
     pub(crate) const fn with_span(self, span: Span) -> XmlError {
         XmlError(self, Some(span))
     }
--- src/xml/mod.rs
+++ src/xml/mod.rs
@@ -9,15 +9,35 @@ pub use crate::xml::errors::{XmlErrorType, XmlParseSourceError};
 pub(crate) use crate::xml::{errors::XmlError, lexer::XmlToken};
 use crate::{BuildFromLexer, Value};
 
+/// A complete XML plist document
+///
+/// Expects the proper song & dance:
+///
+/// ```xml
+/// <?xml version="1.0" encoding="UTF-8"?>
+/// <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+/// <plist version="1.0">
+/// ...
+/// </plist>
+/// ```
+///
+/// If you're just operating on a snippet, use [`Value`] directly
 #[derive(Debug, Clone, PartialEq)]
 pub struct XmlDocument<'a> {
+    /// The XML header
     pub header: XmlHeader<'a>,
+    /// The plist version (entirely ignored/disregarded by the lexer & parser)
     pub plist_version: &'a str,
+    /// What you actually want: the inner plist value
     pub content: Value<'a>,
 }
 
 impl<'a> XmlDocument<'a> {
-    // Can't implement trait because lifetimes
+    /// Parse an `XmlDocument` from a string
+    ///
+    /// (Note: sorry this isn't a [`FromStr`](std::str::FromStr) implementation,
+    /// but that trait doesn't support lifetimes)
+    // TODO: TryFrom impl
     #[allow(clippy::should_implement_trait)]
     pub fn from_str(source: &'a str) -> Result<Self, XmlParseSourceError<'a>> {
         let mut token_iter = XmlToken::lexer(source).spanned().peekable();
@@ -65,13 +85,28 @@ impl<'a> XmlDocument<'a> {
     }
 }
 
+/// The standard blurb at the start of an XML file
+///
+/// Something like:
+/// ```xml
+/// <?xml version="1.0" encoding="UTF-8"?>
+/// ```
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct XmlHeader<'a> {
+    /// The XML version the document is using (the lexer & parser do not use
+    /// this information in any way)
     pub version: &'a str,
+    /// The encoding the document is using (the lexer & parser are designed to
+    /// work on UTF-8 i.e. standard Rust string types, and do not respect this
+    /// field)
     pub encoding: &'a str,
 }
 
 impl<'a> XmlHeader<'a> {
+    /// Cursed regex matching to extract XML version & encoding
+    ///
+    /// Assumes the lexer uses the same regex, and will panic if the regex
+    /// doesn't match (as it already should have)
     fn parse_from_lexer(lex: &mut Lexer<'a, XmlToken<'a>>) -> Self {
         let regex = Regex::new(r#"<\?xml\s+version\s*=\s*"([^"]*)"\s*encoding\s*=\s*"([^"]*)"\s*\?>"#).unwrap();
         let Some((_full, [version, encoding])) =
@@ -83,15 +118,19 @@ impl<'a> XmlHeader<'a> {
     }
 }
 
+/// A trait to allow for the implementation of convenience methods on token
+/// iterator values
 trait TokenIterValueExt {
     type Output;
 
+    /// Provide the source to the error, if present
     fn map_err_to_src(
         self,
         source: &str,
     ) -> Result<Self::Output, XmlParseSourceError>;
 }
 
+/// Useful on the return type of [`BuildFromLexer::build_from_tokens`]
 impl<T> TokenIterValueExt for Result<T, XmlError> {
     type Output = T;
 
@@ -103,6 +142,7 @@ impl<T> TokenIterValueExt for Result<T, XmlError> {
     }
 }
 
+/// Useful on the return type of `token_iter.next()`
 impl<'a> TokenIterValueExt for Option<(Result<XmlToken<'a>, XmlError>, Span)> {
     type Output = (XmlToken<'a>, Span);
 
