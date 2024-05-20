7271f05af6fe51831005f1ca794b7c975b3ad7b2
alpha-tango-kilo <git@heyatk.com>
2024-05-20 17:53:11 +0100

    Initial error handling
    + better data validation

---
+++ src/ascii/errors.rs
@@ -0,0 +1,37 @@
+use logos::Span;
+use thiserror::Error;
+
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+pub(crate) struct AsciiError(AsciiErrorType, Option<Span>);
+
+/// The underlying error encountered when parsing a [`Value`](crate::Value)
+#[derive(Debug, Error, Copy, Clone, Default, PartialEq, Eq)]
+pub enum AsciiErrorType {
+    /// Unlexable content - something is so wrong I don't even know what I'm
+    /// looking at
+    #[default]
+    #[error("unlexable content")]
+    Unlexable,
+    // Lexer errors
+    /// Unclosed quoted string
+    #[error("unclosed quoted string")]
+    UnclosedString,
+    /// Invalid character in data (not hexadecimal or whitespace)
+    #[error(
+        "invalid character in data {0:?} (should be hexadecimal/whitespace)"
+    )]
+    InvalidDataCharacter(char),
+    /// Invalid data length (must have an even number of hexadecimal
+    /// characters)
+    #[error("data is undecodable")]
+    InvalidDataLen,
+    /// Unclosed data
+    #[error("unclosed data")]
+    UnclosedData,
+}
+
+impl AsciiErrorType {
+    pub(crate) const fn with_span(self, span: Span) -> AsciiError {
+        AsciiError(self, Some(span))
+    }
+}
--- src/ascii/mod.rs
+++ src/ascii/mod.rs
@@ -2,11 +2,15 @@
 
 // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/PropertyLists/OldStylePlists/OldStylePLists.html#//apple_ref/doc/uid/20001012-BBCBDBJE
 
+mod errors;
+
+use errors::AsciiError;
+pub use errors::AsciiErrorType;
 use logos::{Lexer, Logos};
 
 // TODO: re-use hierarchy tracker for arrays & dictionaries
 #[derive(Logos, Copy, Clone, Debug, PartialEq)]
-#[logos(skip r"[ \t\r\n\f]+")]
+#[logos(skip r"[ \t\r\n\f]+", error = AsciiError)]
 pub(crate) enum AsciiToken<'a> {
     #[token("(")]
     StartArray,
@@ -33,39 +37,65 @@ pub(crate) enum AsciiToken<'a> {
 
 fn gobble_quoted_string<'a>(
     lexer: &mut Lexer<'a, AsciiToken<'a>>,
-) -> Result<&'a str, ()> {
+) -> Result<&'a str, AsciiError> {
     let rest = lexer.remainder();
     // close_quote is relative to the remainder
     // TODO: consider char windows? https://stackoverflow.com/a/51261570
     // TODO: what if the backslash is escaped? do you even need to do that in
-    // ASCII plist?
+    //       ASCII plist?
     let close_quote = rest
         .chars()
         .zip(rest.char_indices().skip(1))
         .find_map(|(char_one, (index, char_two))| {
             (char_two == '"' && char_one != '\\').then_some(index)
         })
-        .ok_or_else(|| todo!("unclosed string"))?;
+        .ok_or_else(|| {
+            let start_span = lexer.span().start;
+            let end_span = start_span + lexer.remainder().len();
+            AsciiErrorType::UnclosedString.with_span(start_span..end_span)
+        })?;
     lexer.bump(close_quote + 1);
     Ok(&rest[..close_quote])
 }
 
+// Validates the data string's characters & length
 fn gobble_data<'a>(
     lexer: &mut Lexer<'a, AsciiToken<'a>>,
-) -> Result<&'a str, ()> {
+) -> Result<&'a str, AsciiError> {
     let rest = lexer.remainder();
     let mut end_index = None;
+    let mut data_len = 0usize;
     for (index, char) in rest.chars().enumerate() {
         if char == '>' {
             end_index = Some(index);
             break;
-        } else if !(char.is_ascii_whitespace() || char.is_ascii_hexdigit()) {
-            todo!("invalid data char");
+        } else if char.is_ascii_whitespace() || char.is_ascii_hexdigit() {
+            data_len += 1;
+        } else {
+            // Plus one to get inside the '<', and then use `index` for
+            // relative position
+            let span_start = lexer.span().start + 1 + index;
+            let span_end = span_start + 1;
+            return Err(AsciiErrorType::InvalidDataCharacter(char)
+                .with_span(span_start..span_end));
         }
     }
     let Some(end_index) = end_index else {
-        todo!("unclosed data")
+        let start_span = lexer.span().start;
+        let end_span = start_span + lexer.remainder().len();
+        return Err(
+            AsciiErrorType::UnclosedData.with_span(start_span..end_span)
+        );
     };
+    if data_len % 2 != 0 {
+        let start_span = lexer.span().start;
+        // Plus one each time for the start/end '<' '>'
+        let end_span = start_span + 1 + data_len + 1;
+        return Err(
+            AsciiErrorType::InvalidDataLen.with_span(start_span..end_span)
+        );
+    }
+    // Plus one to get past the '>'
     lexer.bump(end_index + 1);
     Ok(&rest[..end_index])
 }
@@ -79,13 +109,13 @@ mod unit_tests {
         for token in AsciiToken::lexer(input) {
             match token {
                 Ok(token) => tokens.push(token),
-                // TODO: display errors once implemented
-                Err(_) => {
+                Err(why) => {
                     eprintln!("Partially lexed: {tokens:#?}");
+                    // TODO: use nice error messages once available
                     // Boilerplate to get nice miette errors in panic messages
                     // let why = lex_error.with_source(input);
                     // print_miette(&why);
-                    panic!("failed to lex");
+                    panic!("failed to lex: {why:?}");
                 },
             }
         }
